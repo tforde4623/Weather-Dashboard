e628f215cbddcf3f17d0f7d797689fc5619d692e
tt4623 <sltebr33zy@gmail.com>
2020-11-06 21:45:21 -0600

    finished most of the function to call api and set 5 day weather

--- assets/js/index.js
+++ assets/js/index.js
@@ -26,7 +26,6 @@ $cityBtn.on('click', (e) => {
             console.log(err)
         })
         .done(res => {
-            console.log(res);
             setCurrent(res);
         });
 });
@@ -49,6 +48,9 @@ function setCurrent(data) {
     // set UV index in DOM by calling func (since it needs a WHOLE DIFFERENT AJAX CALL)
     // when calling function, it should take the lat and lon of the previously called api so exactly same place
     setUvIndex(data.coord.lat, data.coord.lon);
+
+    // call function to set 5 day weather
+    setWeeklyWeather(data.coord.lat, data.coord.lon)
 }
 
 // function for setting UV index
@@ -62,5 +64,32 @@ function setUvIndex(lat, lon) {
             // set DOM to uv index value from response when done
             // maybe change to html and do stuff depending on color
             $currentUvIndex.text(`UV Index: ${res.value}`);
+        });
+}
+
+// function to set 5 day weather for current city
+function setWeeklyWeather(lat, lon) {
+    // ajax call to get 5 day weather
+    $.ajax({
+        type: 'GET',
+        url: 'https://api.openweathermap.org/data/2.5/onecall?lat='+ lat +'&lon='+ lon +'&units=imperial&exclude=hourly,minutely,alerts,curent&appid=36be3a17aed933ccf53d10149b72f6fb'
+    })
+        .fail(err => console.log(err))
+        .done(res => {
+            // set each days data in DOM
+            for (let i = 1; i <= 5; i++) {
+                // for loop to loop through daily forecast (starting at 1 cause array in res gives current day as 0)
+                // need to turn unix stamp given by api to an actual readable date using moment format
+                let dayDate = moment.unix(res.daily[i].dt).format("MM/D/YYYY");
+                // current temp using array index
+                let dayTemp = res.daily[i].temp.day;
+                // current humidity
+                let dayHum = `${res.daily[i].humidity} %`;
+                
+                // set html elements using str templates to corrospond with day were referencing from api response since the ids and elements have same num, kinda cool
+                $(`#day${i}-date`).text(dayDate);
+                $(`#day${i}-temp`).text(`Temp: ${dayTemp}`);
+                $(`#day${i}-hum`).text(`Humidity: ${dayHum}`);
+            }
         });
 }
